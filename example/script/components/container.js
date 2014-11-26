018057dab6b395e8101dcbad7af62b386cb04e1c
Dom Charlesworth <dgc336@gmail.com>
2014-11-26 21:50:54 +0000

    Added text to signify these as placeholder classes

---
+++ example/script/components/container.js
@@ -0,0 +1,4 @@
+/*
+ * This is just a blank file used as a placeholder for a secondary class.
+ * This script will find this and resolve the paths needed to require this class, regardless of contents
+ */
