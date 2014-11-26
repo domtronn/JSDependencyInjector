7b02fb19da3414c98d72f061600696066698ba9f
Dom Charlesworth <dgc336@gmail.com>
2014-11-26 21:51:16 +0000

    Added comment to tell user to try out the package

--- example/script/example.js
+++ example/script/example.js
@@ -6,6 +6,8 @@ require.def("js-dependency-injector/example",
 		return ({
 
 			init: function () {
+				// Try it out on these two classes!!
+				// Place your cursor over one of them and hit Cmd/Ctrl + B
 				var x = new Image();
 				var y = new Container();
 			}
